2dc6b02ae62f3adb1ae97e0b5162bfcc349ed721
Joshua0209 <b10202012@ntu.edu.tw>
2023-11-05 15:22:06 +0800

    typing docs

--- src/Inklink_server/app/models.py
+++ src/Inklink_server/app/models.py
@@ -1,17 +1,18 @@
 from sqlalchemy import Column, Float, ForeignKey, Integer, String, DateTime, Text, ForeignKeyConstraint, CheckConstraint
 from sqlalchemy.orm import relationship
 from database import Base
+import datetime
 
 class User(Base):
     __tablename__ = 'user'
 
-    user_id = Column(Integer, primary_key=True, autoincrement=True)
-    username = Column(String(20), nullable=False, unique=True)
-    password = Column(String(15), nullable=False)
-    email = Column(String(50), nullable=False)
-    phone_number = Column(String(10), nullable=False)
-    agg_rating = Column(Float)
-    role = Column(String(10), nullable=False, server_default="user")
+    user_id: int = Column(Integer, primary_key=True, autoincrement=True)
+    username: str = Column(String(20), nullable=False, unique=True)
+    password: str = Column(String(15), nullable=False)
+    email: str = Column(String(50), nullable=False)
+    phone_number: str = Column(String(10), nullable=False)
+    agg_rating: float = Column(Float)
+    role: str = Column(String(10), nullable=False, server_default="user")
     
     __table_args__ = (
         CheckConstraint(role.in_(['user', 'admin']), name='role_check'),
@@ -20,9 +21,9 @@ class User(Base):
 class Book(Base):
     __tablename__ = 'book'
 
-    isbn = Column(String(13), primary_key=True)
-    title = Column(String(255), nullable=False)
-    edition = Column(Integer, nullable=False)
+    isbn: str = Column(String(13), primary_key=True)
+    title: str = Column(String(255), nullable=False)
+    edition: int = Column(Integer, nullable=False)
 
     authors = relationship("BookAuthors", back_populates="book")
     categories = relationship("BookCategories", back_populates="book")
@@ -30,16 +31,16 @@ class Book(Base):
 class BookAuthors(Base):
     __tablename__ = 'book_authors'
 
-    isbn = Column(String(13), ForeignKey('book.isbn'), primary_key=True)
-    author = Column(String(50), primary_key=True)
+    isbn: str = Column(String(13), ForeignKey('book.isbn'), primary_key=True)
+    author: str = Column(String(50), primary_key=True)
 
     book = relationship("Book", back_populates="authors")
 
 class BookCategories(Base):
     __tablename__ = 'book_categories'
 
-    isbn = Column(String(13), ForeignKey('book.isbn'), primary_key=True)
-    category = Column(String(10), primary_key=True)
+    isbn: str = Column(String(13), ForeignKey('book.isbn'), primary_key=True)
+    category: str = Column(String(10), primary_key=True)
 
     book = relationship("Book", back_populates="categories")
     
@@ -50,11 +51,11 @@ class BookCategories(Base):
 class Request(Base):
     __tablename__ = 'request'
 
-    request_id = Column(Integer, primary_key=True, autoincrement=True)
-    status = Column(String(15), nullable=False)
-    posting_time = Column(DateTime, nullable=False)
-    poster_id = Column(Integer, ForeignKey('user.user_id'), nullable=False)
-    is_type = Column(String(10), nullable=False)
+    request_id: int = Column(Integer, primary_key=True, autoincrement=True)
+    status: str = Column(String(15), nullable=False)
+    posting_time: datetime = Column(DateTime, nullable=False)
+    poster_id: int = Column(Integer, ForeignKey('user.user_id'), nullable=False)
+    is_type: str = Column(String(10), nullable=False)
 
     selling_requests = relationship("SellingRequest", back_populates="request")
     exchange_requests = relationship("ExchangeRequest", back_populates="request")
@@ -67,31 +68,31 @@ class Request(Base):
 class SellingRequest(Base):
     __tablename__ = 'selling_request'
 
-    request_id = Column(Integer, ForeignKey('request.request_id'), primary_key=True)
-    price = Column(Integer, nullable=False)
-    buyer_id = Column(Integer, ForeignKey('user.user_id'))
-    buying_time = Column(DateTime)
+    request_id: int = Column(Integer, ForeignKey('request.request_id'), primary_key=True)
+    price: int = Column(Integer, nullable=False)
+    buyer_id: int = Column(Integer, ForeignKey('user.user_id'))
+    buying_time: datetime = Column(DateTime)
 
     request = relationship("Request", back_populates="selling_requests")
 
 class ExchangeRequest(Base):
     __tablename__ = 'exchange_request'
 
-    request_id = Column(Integer, ForeignKey('request.request_id'), primary_key=True)
-    isbn = Column(String(13), ForeignKey('book.isbn'), primary_key=True)
-    no_of_copies = Column(Integer, nullable=False)
-    book_condition = Column(String(10), nullable=False)
+    request_id: int = Column(Integer, ForeignKey('request.request_id'), primary_key=True)
+    isbn: str = Column(String(13), ForeignKey('book.isbn'), primary_key=True)
+    no_of_copies: int = Column(Integer, nullable=False)
+    book_condition: str = Column(String(10), nullable=False)
 
     request = relationship("Request", back_populates="exchange_requests")
 
 class ExchangeResponse(Base):
     __tablename__ = 'exchange_response'
 
-    response_id = Column(Integer, primary_key=True)
-    status = Column(String(15), nullable=False)
-    response_time = Column(DateTime, nullable=False)
-    responder_id = Column(Integer, ForeignKey('user.user_id'))
-    request_id = Column(Integer, ForeignKey('request.request_id'), primary_key=True)
+    response_id: int = Column(Integer, primary_key=True)
+    status: str = Column(String(15), nullable=False)
+    response_time: datetime = Column(DateTime, nullable=False)
+    responder_id: int = Column(Integer, ForeignKey('user.user_id'))
+    request_id: int = Column(Integer, ForeignKey('request.request_id'), primary_key=True)
     
     __table_args__ = (
         CheckConstraint(status.in_(['Accepted', 'Deleted', 'Rejected', 'Available']), name='status_check'),
@@ -100,11 +101,11 @@ class ExchangeResponse(Base):
 class ProposeToExchange(Base):
     __tablename__ = 'propose_to_exchange'
 
-    response_id = Column(Integer, primary_key=True)
-    isbn = Column(String(13), ForeignKey('book.isbn'), primary_key=True)
-    no_of_copies = Column(Integer, nullable=False)
-    book_condition = Column(String(10), nullable=False)
-    request_id = Column(Integer, primary_key=True)
+    response_id: int = Column(Integer, primary_key=True)
+    isbn: str = Column(String(13), ForeignKey('book.isbn'), primary_key=True)
+    no_of_copies: int = Column(Integer, nullable=False)
+    book_condition: str = Column(String(10), nullable=False)
+    request_id: int = Column(Integer, primary_key=True)
 
     __table_args__ = (
         ForeignKeyConstraint([response_id, request_id], [ExchangeResponse.response_id, ExchangeResponse.request_id]),
@@ -113,10 +114,10 @@ class ProposeToExchange(Base):
 class SellExchange(Base):
     __tablename__ = 'sell_exchange'
 
-    request_id = Column(Integer, ForeignKey('request.request_id'), primary_key=True)
-    isbn = Column(String(13), ForeignKey('book.isbn'), primary_key=True)
-    no_of_copies = Column(Integer, nullable=False)
-    book_condition = Column(String(10), nullable=False)
+    request_id: int = Column(Integer, ForeignKey('request.request_id'), primary_key=True)
+    isbn: str = Column(String(13), ForeignKey('book.isbn'), primary_key=True)
+    no_of_copies: int = Column(Integer, nullable=False)
+    book_condition: str = Column(String(10), nullable=False)
 
     request = relationship("Request")
     book = relationship("Book")
@@ -124,18 +125,18 @@ class SellExchange(Base):
 class Owns(Base):
     __tablename__ = 'owns'
 
-    owner_id = Column(Integer, ForeignKey('user.user_id'), primary_key=True)
-    isbn = Column(String(13), ForeignKey('book.isbn'), primary_key=True)
-    no_of_copies = Column(Integer, nullable=False)
+    owner_id: int = Column(Integer, ForeignKey('user.user_id'), primary_key=True)
+    isbn: str = Column(String(13), ForeignKey('book.isbn'), primary_key=True)
+    no_of_copies: int = Column(Integer, nullable=False)
 
 class Rating(Base):
     __tablename__ = 'rating'
 
-    rating_user_id = Column(Integer, ForeignKey('user.user_id'), primary_key=True)
-    rated_user_id = Column(Integer, ForeignKey('user.user_id'), primary_key=True)
-    request_id = Column(Integer, ForeignKey('request.request_id'), primary_key=True)
-    rating_time = Column(DateTime, nullable=False)
-    score = Column(Integer, nullable=False)
+    rating_user_id: int = Column(Integer, ForeignKey('user.user_id'), primary_key=True)
+    rated_user_id: int = Column(Integer, ForeignKey('user.user_id'), primary_key=True)
+    request_id: int = Column(Integer, ForeignKey('request.request_id'), primary_key=True)
+    rating_time: datetime = Column(DateTime, nullable=False)
+    score: int = Column(Integer, nullable=False)
 
     rated_user = relationship("User", foreign_keys=[rated_user_id])
     rating_user = relationship("User", foreign_keys=[rating_user_id])
