9753f05ab7c9d5ba7979179ff60f062dc965eeb7
Joshua0209 <b10202012@ntu.edu.tw>
2023-11-05 14:18:44 +0800

    data dictionary version, check constraints added

--- src/Inklink_server/app/models.py
+++ src/Inklink_server/app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Float, ForeignKey, Integer, String, DateTime, Text, ForeignKeyConstraint
+from sqlalchemy import Column, Float, ForeignKey, Integer, String, DateTime, Text, ForeignKeyConstraint, CheckConstraint
 from sqlalchemy.orm import relationship
 from database import Base
 
@@ -12,17 +12,10 @@ class User(Base):
     phone_number = Column(String(10), nullable=False)
     agg_rating = Column(Float)
     role = Column(String(10), nullable=False, server_default="user")
-
-class Rating(Base):
-    __tablename__ = 'rating'
-
-    rating_user_id = Column(Integer, ForeignKey('user.user_id'), primary_key=True)
-    rated_user_id = Column(Integer, ForeignKey('user.user_id'), primary_key=True)
-    request_id = Column(Integer, ForeignKey('request.request_id'), primary_key=True)
-    rating_time = Column(DateTime, nullable=False)
-    score = Column(Integer, nullable=False)
-
-    rated_user = relationship("User", foreign_keys=[rated_user_id])
+    
+    __table_args__ = (
+        CheckConstraint(role.in_(['user', 'admin']), name='role_check'),
+    )
 
 class Request(Base):
     __tablename__ = 'request'
@@ -35,6 +28,11 @@ class Request(Base):
 
     selling_requests = relationship("SellingRequest", back_populates="request")
     exchange_requests = relationship("ExchangeRequest", back_populates="request")
+    
+    __table_args__ = (
+        CheckConstraint(status.in_(['Accepted', 'Deleted', 'Remained']), name='status_check'),
+        CheckConstraint(is_type.in_(['Exchange', 'Sell']), name='is_type_check'),
+    )
 
 class SellingRequest(Base):
     __tablename__ = 'selling_request'
@@ -54,6 +52,32 @@ class ExchangeRequest(Base):
 
     request = relationship("Request", back_populates="exchange_requests")
 
+class ExchangeResponse(Base):
+    __tablename__ = 'exchange_response'
+
+    response_id = Column(Integer, primary_key=True, unique=True)
+    status = Column(String(15), nullable=False)
+    response_time = Column(DateTime, nullable=False)
+    responder_id = Column(Integer, ForeignKey('user.user_id'))
+    request_id = Column(Integer, ForeignKey('exchange_request.request_id'), primary_key=True)
+    
+    __table_args__ = (
+        CheckConstraint(status.in_(['Accepted', 'Deleted', 'Rejected', 'Available']), name='status_check'),
+    )
+
+class ProposeToExchange(Base):
+    __tablename__ = 'propose_to_exchange'
+
+    response_id = Column(Integer, primary_key=True)
+    isbn = Column(String(13), ForeignKey('book.isbn'), primary_key=True)
+    no_of_copies = Column(Integer, nullable=False)
+    book_condition = Column(String(10), nullable=False)
+    request_id = Column(Integer, primary_key=True)
+
+    __table_args__ = (
+        ForeignKeyConstraint([response_id, request_id], [ExchangeResponse.response_id, ExchangeResponse.request_id],{}),
+    )
+
 class SellExchange(Base):
     __tablename__ = 'sell_exchange'
 
@@ -64,28 +88,6 @@ class SellExchange(Base):
 
     request = relationship("Request")
     book = relationship("Book")
-
-class ExchangeResponse(Base):
-    __tablename__ = 'exchange_response'
-
-    response_id = Column(Integer, primary_key=True, unique=True)
-    responder_id = Column(Integer, ForeignKey('user.user_id'))
-    request_id = Column(Integer, ForeignKey('exchange_request.request_id'), primary_key=True)
-    status = Column(String(15), nullable=False)
-    response_time = Column(DateTime, nullable=False)
-
-class ProposeToExchange(Base):
-    __tablename__ = 'propose_to_exchange'
-
-    isbn = Column(String(13), ForeignKey('book.isbn'), primary_key=True)
-    no_of_copies = Column(Integer, nullable=False)
-    book_condition = Column(String(10), nullable=False)
-    response_id = Column(Integer, primary_key=True)
-    request_id = Column(Integer, primary_key=True)
-
-    __table_args__ = (
-        ForeignKeyConstraint([response_id, request_id], [ExchangeResponse.response_id, ExchangeResponse.request_id],{}),
-    )
 
 class Book(Base):
     __tablename__ = 'book'
@@ -101,7 +103,7 @@ class BookAuthors(Base):
     __tablename__ = 'book_authors'
 
     isbn = Column(String(13), ForeignKey('book.isbn'), primary_key=True)
-    author = Column(String(), primary_key=True)
+    author = Column(String(50), primary_key=True)
 
     book = relationship("Book", back_populates="authors")
 
@@ -112,6 +114,10 @@ class BookCategories(Base):
     category = Column(String(10), primary_key=True)
 
     book = relationship("Book", back_populates="categories")
+    
+    __table_args__ = (
+        CheckConstraint(category.in_(['textbook', 'others']), name='category_check'), 
+    )
 
 class Owns(Base):
     __tablename__ = 'owns'
@@ -119,3 +125,16 @@ class Owns(Base):
     owner_id = Column(Integer, ForeignKey('user.user_id'), primary_key=True)
     isbn = Column(String(13), ForeignKey('book.isbn'), primary_key=True)
     no_of_copies = Column(Integer, nullable=False)
+
+class Rating(Base):
+    __tablename__ = 'rating'
+
+    rating_user_id = Column(Integer, ForeignKey('user.user_id'), primary_key=True)
+    rated_user_id = Column(Integer, ForeignKey('user.user_id'), primary_key=True)
+    request_id = Column(Integer, ForeignKey('request.request_id'), primary_key=True)
+    rating_time = Column(DateTime, nullable=False)
+    score = Column(Integer, nullable=False)
+
+    rated_user = relationship("User", foreign_keys=[rated_user_id])
+    rating_user = relationship("User", foreign_keys=[rating_user_id])
+    request = relationship("Request", foreign_keys=[request_id])
