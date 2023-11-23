f5000dd79d800f07ffef9e795187b80e72add616
godjojo00 <cyj0430@gmail.com>
2023-11-23 19:51:05 +0800

    header

--- src/Inklink_client/src/App.jsx
+++ src/Inklink_client/src/App.jsx
@@ -1,27 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import BookExchange from './pages/bookexchange'; // 請確保路徑是正確的
-import Home from './pages/Home'; // 請替換路徑為您的 Home 組件的正確路徑
+import BookExchange from './pages/bookexchange';
+import Home from './pages/Home';
 import IdVerify from './pages/IdVerify';
 import QuestionForm from './pages/questionform';
 import SignUp_Page from './pages/signUp_page';
 import Login from './pages/login';
-import Owns from './pages/owns'
+import Owns from './pages/owns';
+import Header from './component/header';
 
 function App() {
+  const [user, setUser] = useState(null); // 使用狀態來管理用戶
+
+  const handleLogin = (userData) => {
+    // 登入後的處理邏輯，設置用戶狀態等
+    setUser(userData);
+  };
+
+  const handleLogout = () => {
+    // 登出後的處理邏輯，清除用戶狀態等
+    setUser(null);
+  };
+
   return (
     <Router>
       <div>
+        <Header user={user} onLogout={handleLogout} /> {/* 將用戶數據和登出函數傳遞給 Header */}
         <Routes>
-          <Route>
-            <Route element={<SignUp_Page/>} path='/SignUp' />
-            <Route element={<Owns/>} path='/owns' />
-            <Route element={<Login/>} path='/login' />
-            <Route element={<IdVerify/>} path='/Id' />
-            <Route element={<Home/>} path="/" exact />
-            <Route element={<BookExchange/>} path="/book-exchange" />
-            <Route element={<QuestionForm/>} path='/questionform' />
-          </Route>
+          {/* 在每個 Route 下渲染相同的 Header */}
+          <Route
+            path="/*"
+            element={
+              <Routes>
+                <Route element={<Home />} path="/" />
+                <Route element={<BookExchange />} path="/book-exchange" />
+                <Route element={<QuestionForm />} path="/questionform" />
+                <Route
+                  element={<SignUp_Page onLogin={handleLogin} />}
+                  path="/signUp"
+                />
+                <Route
+                  element={<Login onLogin={handleLogin} />}
+                  path="/login"
+                />
+                <Route element={<IdVerify />} path="/Id" />
+                <Route element={<Owns />} path="/owns" />
+              </Routes>
+            }
+          />
         </Routes>
       </div>
     </Router>
---
+++ src/Inklink_client/src/component/header.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = ({ isLoggedIn, username }) => {
+  return (
+    <div className="flex justify-between items-center bg-gray-800 text-white p-4">
+      <div className="flex items-center">
+        <Link to="/" className="text-2xl font-bold mr-4">
+          Inklink
+        </Link>
+      </div>
+      <div className="flex items-center">
+        <Link to="/owns" className="mr-4">
+          我的藏書
+        </Link>
+        <Link to="/pages/post" className="mr-4">發文</Link>
+        {isLoggedIn ? (
+          <span className="mr-4">{username}</span>
+        ) : (
+          <Link to="/login">登入</Link>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Header;
