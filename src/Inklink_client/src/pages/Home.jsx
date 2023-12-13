08eb3e96ff87a9790115c1b48d6b6fe6ec5e28a5
徐子晏 <ckocp3@xuziyans-MacBook-Pro.local>
2023-12-13 10:49:59 +0800

    search requests at home

--- src/Inklink_client/src/pages/Home.jsx
+++ src/Inklink_client/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Table, Button, Pagination, Tabs } from 'antd';
+import { Table, Button, Pagination, Tabs, Input } from 'antd';
 import { callApi } from '../utils/axios_client';
 import { Link } from 'react-router-dom';
 
@@ -8,6 +8,8 @@ const { TabPane } = Tabs;
 const Home = () => {
   const [sellPosts, setSellPosts] = useState([]);
   const [exchangePosts, setExchangePosts] = useState([]);
+  const [sellFilters, setSellFilters] = useState({ book_title: '', seller_name: '', price_limit: null });
+  const [exchangeFilters, setExchangeFilters] = useState({ book_title: '', seller_name: '', description: '' });
   const [currentSellPage, setCurrentSellPage] = useState(1);
   const [currentExchangePage, setCurrentExchangePage] = useState(1);
   const [totalSellPosts, setTotalSellPosts] = useState(0);
@@ -29,9 +31,13 @@ const Home = () => {
     return requests;
   };
 
-  const fetchRequests = async (type, currentPage) => {
+  const fetchRequests = async (type, page, filters) => {
     try {
-      const response = await callApi(`http://localhost:8000/requests/${type}?page=${currentPage}&limit=${pageSize}`, 'get');
+      const filteredFilters = Object.fromEntries(
+        Object.entries(filters).filter(([key, value]) => value !== null && value !== '')
+      );
+      const queryParams = new URLSearchParams({ ...filteredFilters, page, limit: pageSize }).toString();
+      const response = await callApi(`http://localhost:8000/requests/${type}?${queryParams}`, 'get');
       const requestIds = response.data.request_list;
       const requests = await fetchRequestDetails(requestIds, type);
       return { requests, totalCount: response.data.total_count };
@@ -42,18 +48,36 @@ const Home = () => {
   };
 
   const fetchSellRequests = async (page) => {
-    const sellResponse = await fetchRequests('sell', page);
+    const sellResponse = await fetchRequests('sell', page, sellFilters);
     setSellPosts(sellResponse.requests || []);
     setTotalSellPosts(sellResponse.totalCount || 0);
     setCurrentSellPage(page);
   };
 
   const fetchExchangeRequests = async (page) => {
-    const exchangeResponse = await fetchRequests('exchange', page);
+    const exchangeResponse = await fetchRequests('exchange', page, exchangeFilters);
     setExchangePosts(exchangeResponse.requests || []);
     setTotalExchangePosts(exchangeResponse.totalCount || 0);
     setCurrentExchangePage(page);
-  }
+  };
+
+  const onSellFilterChange = (e) => {
+    setSellFilters({ ...sellFilters, [e.target.name]: e.target.value });
+  };
+
+  const onExchangeFilterChange = (e) => {
+    setExchangeFilters({ ...exchangeFilters, [e.target.name]: e.target.value });
+  };
+
+  const onSellSearch = () => {
+    setCurrentSellPage(1);
+    fetchSellRequests(1);
+  };
+
+  const onExchangeSearch = () => {
+    setCurrentExchangePage(1);
+    fetchExchangeRequests(1);
+  };
 
   const onSellPageChange = (page) => {
     fetchSellRequests(page);
@@ -133,6 +157,31 @@ const Home = () => {
     <div className="container mx-auto mt-8">
       <Tabs defaultActiveKey="1">
         <TabPane tab="Selling Requests" key="1">
+          <div>
+            <Input
+              placeholder="Search by book title"
+              name="book_title"
+              value={sellFilters.book_title}
+              onChange={onSellFilterChange}
+              style={{ margin: '0 10px 10px 0' }}
+            />
+            <Input
+              placeholder="Search by poster name"
+              name="seller_name"
+              value={sellFilters.seller_name}
+              onChange={onSellFilterChange}
+              style={{ margin: '0 10px 10px 0' }}
+            />
+            <Input
+              placeholder="Search by price limit"
+              name="price_limit"
+              value={sellFilters.price_limit}
+              onChange={onSellFilterChange}
+              style={{ margin: '0 10px 10px 0' }}
+              type="number"
+            />
+            <Button onClick={onSellSearch}>Search</Button>
+          </div>
           <Table dataSource={sellPosts} columns={sellColumns} rowKey="request_id" />
           <Pagination
             current={currentSellPage}
@@ -142,6 +191,30 @@ const Home = () => {
           />
         </TabPane>
         <TabPane tab="Exchange Requests" key="2">
+          <div>
+            <Input
+              placeholder="Search by book title"
+              name="book_title"
+              value={exchangeFilters.book_title}
+              onChange={onExchangeFilterChange}
+              style={{ margin: '0 10px 10px 0' }}
+            />
+            <Input
+              placeholder="Search by poster name"
+              name="seller_name"
+              value={exchangeFilters.seller_name}
+              onChange={onExchangeFilterChange}
+              style={{ margin: '0 10px 10px 0' }}
+            />
+            <Input
+              placeholder="Search by wishlist description"
+              name="description"
+              value={exchangeFilters.description}
+              onChange={onExchangeFilterChange}
+              style={{ margin: '0 10px 10px 0' }}
+            />
+            <Button onClick={onExchangeSearch}>Search</Button>
+          </div>
           <Table dataSource={exchangePosts} columns={exchangeColumns} rowKey="request_id" />
           <Pagination
             current={currentExchangePage}
